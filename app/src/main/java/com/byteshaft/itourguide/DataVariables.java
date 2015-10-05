2244e37091ac7fce105887c275849daecfdcb996
Mohsin Falak <fi8er1@gmail.com>
2015-10-05 18:05:01 +0500

    ListView populated by DataVariables

---
+++ app/src/main/java/com/byteshaft/itourguide/DataVariables.java
@@ -0,0 +1,14 @@
+package com.byteshaft.itourguide;
+
+public class DataVariables {
+    
+    static String[] one = new String[] {"Minar-e-Pakistan",
+            "A public monument located in Iqbal Park which is one of the largest urban parks in Lahore",
+            "31.592513, 74.309799"};
+    static String[] two = new String[] {"Baadshahi Mosque",
+            "Commissioned by the sixth Mughal Emperor Aurangzeb. Constructed between 1671 and 1673",
+            "31.588563, 74.311620"};
+    static String[] three = new String[] {"Lahore Fort",
+            "Locally referred to as Shahi Qila, is a citadel in the city of Lahore",
+            "31.587890, 74.315288"};
+}
