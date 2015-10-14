d3a9f84603e4cc24426163c587b38e029c5e43bb
Imran <linuxholic@hotmail.com>
2015-10-14 14:21:29 +0500

    Implement Navigation

--- app/src/main/java/com/byteshaft/itourguide/MapsActivity.java
+++ app/src/main/java/com/byteshaft/itourguide/MapsActivity.java
@@ -1,22 +1,36 @@
 package com.byteshaft.itourguide;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.directions.route.Route;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    public static GoogleMap mMap;
     Button satelliteButton;
     Button normalButton;
+    LatLng start;
+    LatLng end;
+    LatLng wayPoint;
+    LatLng targetLocation;
+    LatLng currentLocation;
+    RoutingListener routingListener;
+    public static MarkerOptions a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +40,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        MainActivity.isMapActivityOpen = true;
+        LocationService locationService = new LocationService(AppGlobals.getContext());
+        routingListener = new RoutingListener() {
+            @Override
+            public void onRoutingFailure() {
+
+            }
+
+            @Override
+            public void onRoutingStart() {
+
+            }
+
+            @Override
+            public void onRoutingSuccess(PolylineOptions polylineOptions, Route route) {
+                PolylineOptions polyoptions = new PolylineOptions();
+                polyoptions.color(Color.BLUE);
+                polyoptions.width(10);
+                polylineOptions.zIndex(102);
+                polyoptions.addAll(polylineOptions.getPoints());
+                mMap.addPolyline(polyoptions);
+            }
+
+            @Override
+            public void onRoutingCancelled() {
+
+            }
+        };
+        start = new LatLng(LocationService.latitude, LocationService.longitude);
+        end = new LatLng(MainActivity.finalLat, MainActivity.finalLon);
         satelliteButton = (Button) findViewById(R.id.satelliteButton);
         normalButton = (Button) findViewById(R.id.normalButton);
     }
@@ -33,10 +77,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng currentLocation = new LatLng(MainActivity.finalLat, MainActivity.finalLon);
-        mMap.addMarker(new MarkerOptions().position(currentLocation).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+        targetLocation = new LatLng(MainActivity.finalLat, MainActivity.finalLon);
+        currentLocation = new LatLng(LocationService.latitude, LocationService.longitude);
+        a = new MarkerOptions().position(targetLocation);
+        mMap.addMarker(a);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(targetLocation));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(MainActivity.finalLat, MainActivity.finalLon), 16.0f));
+        Routing routing = new Routing.Builder().travelMode(Routing.TravelMode.WALKING)
+                .withListener(routingListener).waypoints(start, end).build();
+        routing.execute();
         satelliteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
